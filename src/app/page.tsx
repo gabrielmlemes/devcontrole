aa44f874e40b2b4e5f5ddc9788f7675b35095a2a
Gabriel Lemes <g.moreiralemess@gmail.com>
2024-11-03 21:04:46 -0300

    feat: add Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import ImgHome from "@/assets/hero.svg";
+
 const Home = () => {
   return (
-    <div>
-      <h1>HOME</h1>
-    </div>
+    <main className="w-full flex flex-col justify-center items-center min-h-[calc(100vh-80px)]">
+      <h1 className="font-medium text-3xl md:text-5xl mb-10 text-blue-600 uppercase">
+        Gerencie sua empresa
+      </h1>
+
+      <Image
+        src={ImgHome}
+        alt="Imagem Home"
+        width={600}
+        className="max-w-sm md:max-w-xl"
+      />
+    </main>
   );
 };
 

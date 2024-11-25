914d747dbbb38e871f3a6af05e0ede997d4a1890
Gabriel Lemes <g.moreiralemess@gmail.com>
2024-11-24 23:44:55 -0300

    feat: add POST route to customer

---
+++ src/app/api/customer/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "Not authorized" }, { status: 401 });
+  }
+
+  const { name, email, phone, adress, userId } = await req.json();
+
+  try {
+    await prisma.customer.create({
+      data: {
+        name: name,
+        email: email,
+        phone: phone,
+        address: adress ? adress : "",
+        userId: userId,
+      },
+    });
+
+    return NextResponse.json({ message: "CLIENTE CADASTRADO" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to created new customer" },
+      { status: 400 }
+    );
+  }
+}
